4fad098a161377e8875fd79f87ca2ccd7e860018
Vamonosraza <martinezjessy393@gmail.com>
2024-07-14 15:12:41 -0700

    feat: Add HeroPage and InfoBoxes components
    The code changes include:
    - Added a new HeroPage component for the homepage
    - Added a new InfoBoxes component for displaying information boxes on the homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
+import HeroPage from '@/components/Hero'
+import InfoBoxes from '@/components/InfoBoxes'
 
 const HomePage = () => {
-  return (
+  return (<>
+  <HeroPage/>
+  <InfoBoxes/>
     <div className='text-4xl'>
       <h1 className="">Welcome</h1>
       <Link href="/properties" className="btn">Show Properties</Link>
     </div>
+    </>
   )
 }
 
