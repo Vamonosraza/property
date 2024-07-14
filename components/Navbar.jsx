adb9aa6053b7c099f0ce0d2801ff679b9f2b94ee
Vamonosraza <martinezjessy393@gmail.com>
2024-07-14 14:05:44 -0700

    feat: Improve Navbar component with dynamic menu options and login/logout functionality

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,11 +6,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { FaGoogle } from 'react-icons/fa'
 import { useState } from 'react'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
 
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const pathname = usePathname()
 
     return (
         <nav className="bg-blue-700 border-b border-blue-500">
@@ -65,23 +69,27 @@ const Navbar = () => {
                             <div className="flex space-x-2">
                                 <Link
                                     href="/"
-                                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                    className={ `${pathname === '/' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} rounded-md px-3 py-2`} 
                                 >Home</Link>
 
                                 <Link
                                     href="/properties"
-                                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                    className={ `${pathname === '/properties' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} rounded-md px-3 py-2`}
                                 >Properties</Link>
 
-                                <Link
-                                    href="/properties/add"
-                                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                                >Add Property</Link>
+                                { isLoggedIn && (
+                                    <Link
+                                        href="/properties/add"
+                                        className={ `${pathname === '/properties/add' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} rounded-md px-3 py-2`}
+                                    >Add Property</Link>
+
+                                )}
 
                             </div>
                         </div>
                     </div>
 
+                    { !isLoggedIn && (
 
                     <div className="hidden md:block md:ml-6">
                         <div className="flex items-center">
@@ -93,7 +101,9 @@ const Navbar = () => {
                             </button>
                         </div>
                     </div>
+                    )}
 
+                    { isLoggedIn && (
 
                     <div
                         className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
@@ -184,6 +194,7 @@ const Navbar = () => {
                             )}
                         </div>
                     </div>
+                    )}
                 </div>
             </div>
 
@@ -191,25 +202,31 @@ const Navbar = () => {
                 <div className="space-y-1 px-2 pb-3 pt-2">
                     <Link
                         href="/"
-                        className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+                        className={`${pathname === '/'? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} block rounded-md px-3 py-2 text-base font-medium`}
                     >Home</Link>
 
                     <Link
                         href="/properties"
-                        className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                        className={`${pathname === '/properties'? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} block rounded-md px-3 py-2 text-base font-medium`}
                     >Properties</Link>
+
+                    { isLoggedIn && (
 
                     <Link
                         href="/properties/add"
-                        className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                        className={`${pathname === '/properties/add'? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} block rounded-md px-3 py-2 text-base font-medium`}
                     >Add Property</Link>
+                    )}
+
+                    { !isLoggedIn && (
 
                     <button
                         className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
                     >
-                        <FaGoogle className="text-white mr-2" />
+                        {/* <FaGoogle className="text-white mr-2" /> */}
                         <span>Login or Register</span>
                     </button>
+                    )}
                 </div>
             </div>)}
             
