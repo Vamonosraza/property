e1c72e3b63659de609d56e37aa23e1177bfae048
Vamonosraza <martinezjessy393@gmail.com>
2024-07-13 22:03:28 -0700

    chore: Update npm dependencies for autoprefixer and postcss
    Working now

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,13 +19,13 @@ const Navbar = () => {
                                 className='block h-6 w-6'
                                 fill='none'
                                 viewBox='0 0 24 24'
-                                stroke-width='1.5'
+                                strokeWidth='1.5'
                                 stroke='currentColor'
                                 aria-hidden='true'>
 
                                 <path
-                                    stroke-linecap='round'
-                                    stroke-linejoin='round'
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
                                     d='M4 6h16M4 12h16M4 18h16'/>
                             </svg>
                         </button>
@@ -52,37 +52,37 @@ const Navbar = () => {
                         </div>
                     </div>
 
-                    <div class="hidden md:block md:ml-6">
-                        <div class="flex items-center">
+                    <div className="hidden md:block md:ml-6">
+                        <div className="flex items-center">
                             <button
-                            class="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-                                <i class="fa-brands fa-google text-white mr-2"></i>
+                            className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                                <i className="fa-brands fa-google text-white mr-2"></i>
                                 <span>Login or Register</span>
                             </button>
                         </div>
                     </div>
 
                     <div
-                        class="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
+                        className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
 
-                        <a href="messages.html" class="relative group">
+                        <a href="messages.html" className="relative group">
                             <button
                                 type="button"
-                                class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
 
-                                <span class="absolute -inset-1.5"></span>
-                                <span class="sr-only">View notifications</span>
+                                <span className="absolute -inset-1.5"></span>
+                                <span className="sr-only">View notifications</span>
                                 <svg
-                                    class="h-6 w-6"
+                                    className="h-6 w-6"
                                     fill="none"
                                     viewBox="0 0 24 24"
-                                    stroke-width="1.5"
+                                    strokeWidth="1.5"
                                     stroke="currentColor"
                                     aria-hidden="true">
 
                                     <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
                                         d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"/>
 
                                 </svg>
@@ -94,19 +94,19 @@ const Navbar = () => {
                             </span>
                         </a>
 
-                        <div class="relative ml-3">
+                        <div className="relative ml-3">
                             <div>
                                 <button
                                     type="button"
-                                    class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                    className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                     id="user-menu-button"
                                     aria-expanded="false"
                                     aria-haspopup="true">
 
-                                    <span class="absolute -inset-1.5"></span>
-                                    <span class="sr-only">Open user menu</span>
+                                    <span className="absolute -inset-1.5"></span>
+                                    <span className="sr-only">Open user menu</span>
                                     <img
-                                        class="h-8 w-8 rounded-full"
+                                        className="h-8 w-8 rounded-full"
                                         src="images/profile.png"
                                         alt=""
                                     />
@@ -115,33 +115,33 @@ const Navbar = () => {
 
                             <div
                                 id="user-menu"
-                                class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                className="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                                 role="menu"
                                 aria-orientation="vertical"
                                 aria-labelledby="user-menu-button"
-                                tabindex="-1">
+                                tabIndex="-1">
 
                                 <a
                                     href="/profile.html"
-                                    class="block px-4 py-2 text-sm text-gray-700"
+                                    className="block px-4 py-2 text-sm text-gray-700"
                                     role="menuitem"
-                                    tabindex="-1"
+                                    tabIndex="-1"
                                     id="user-menu-item-0"
                                     >Your Profile</a>
 
                                 <a
                                     href="saved-properties.html"
-                                    class="block px-4 py-2 text-sm text-gray-700"
+                                    className="block px-4 py-2 text-sm text-gray-700"
                                     role="menuitem"
-                                    tabindex="-1"
+                                    tabIndex="-1"
                                     id="user-menu-item-2"
                                     >Saved Properties</a>
 
                                 <a
                                     href="#"
-                                    class="block px-4 py-2 text-sm text-gray-700"
+                                    className="block px-4 py-2 text-sm text-gray-700"
                                     role="menuitem"
-                                    tabindex="-1"
+                                    tabIndex="-1"
                                     id="user-menu-item-2"
                                     >Sign Out</a>
 
