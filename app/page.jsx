20b22024819108a06d72326e4058c9272be61bbf
Vamonosraza <martinezjessy393@gmail.com>
2024-07-14 21:30:23 -0700

    feat: Add HomeProperties component for displaying recent properties on the homepage

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Link from 'next/link'
 import HeroPage from '@/components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
+import HomeProperties from '@/components/HomeProperties'
 
 const HomePage = () => {
   return (<>
   <HeroPage/>
   <InfoBoxes/>
+  <HomeProperties/>
     <div className='text-4xl'>
       <h1 className="">Welcome</h1>
       <Link href="/properties" className="btn">Show Properties</Link>
