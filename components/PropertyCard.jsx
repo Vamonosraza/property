7c0923de041b80c600cb5ea032d2f966dbd9c0c5
Vamonosraza <martinezjessy393@gmail.com>
2024-07-14 21:16:09 -0700

    feat: Add lazy loading for property images in PropertyCard component

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,14 +1,15 @@
     import React from 'react'
     import Image from 'next/image'
     import Link from 'next/link'
+    import { FaBed, FaBath, FaRulerCombined, FaMoneyBill, FaMapMarked } from 'react-icons/fa'
 
     const PropertyCard = ({property}) => {
 const getRate = () =>{
     const {rates} = property
 
-    if(rates.weekly && rates.monthly){
+    if(rates.monthly){
         return `${rates.monthly.toLocaleString()}/mo`
-    }else if (rates.monthly){
+    }else if (rates.weekly){
         return `${rates.weekly.toLocaleString()}/wk`
     }else if (rates.nightly){
         return `${rates.nightly.toLocaleString()}/nite`
@@ -38,41 +39,50 @@ const getRate = () =>{
 
                 <div className="flex justify-center gap-4 text-gray-500 mb-4">
                     <p>
-                    <i className="fa-solid fa-bed"></i> {property.beds}
+                    <FaBed className="inline"/> {property.beds}
                     <span className="md:hidden lg:inline">-Beds</span>
                     </p>
                     <p>
-                    <i className="fa-solid fa-bath"></i> {property.baths}
+                    <FaBath className='inline'/> {property.baths}
                     <span className="md:hidden lg:inline">-Baths</span>
                     </p>
                     <p>
-                    <i className="fa-solid fa-ruler-combined"></i>
-                    {property.square_feet}<span className="md:hidden lg:inline">sqft</span>
+                    <FaRulerCombined className='inline'/> {property.square_feet}
+                    <span className="md:hidden lg:inline">sqft</span>
                     </p>
                 </div>
 
                 <div
                     className="flex justify-center gap-4 text-green-900 text-sm mb-4"
                 >
-                    <p><i className="fa-solid fa-money-bill"></i> Weekly</p>
-                    <p><i className="fa-solid fa-money-bill"></i> Monthly</p>
+                    {property.rates.nightly && (
+                        <p>
+                            <FaMoneyBill className='inline'/> Nightly
+                        </p>
+                    )}
+                    {property.rates.weekly && (
+
+                    <p><FaMoneyBill className='inline'/> Weekly</p>
+                    )}
+                    {property.rates.monthly && (
+
+                    <p><FaMoneyBill className='inline'/> Monthly</p>
+                    )}
                 </div>
 
                 <div className="border border-gray-100 mb-5"></div>
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="flex align-middle gap-2 mb-4 lg:mb-0">
-                    <i
-                        className="fa-solid fa-location-dot text-lg text-orange-700"
-                    ></i>
-                    <span className="text-orange-700"> {property.city} </span>
+                    <FaMapMarked className="inline"/>
+                    <span className="text-orange-700"> {property.location.city} {property.location.state} </span>
                     </div>
-                    <a
-                    href="property.html"
+                    <Link
+                    href={`/properties/${property.id}`}
                     className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                     >
-                    {property.details}
-                    </a>
+                    View Property
+                    </Link>
                 </div>
                 </div>
             </div>
